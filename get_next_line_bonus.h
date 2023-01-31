0b75ce81b5d554f0ce0aa8eb7b1f4df8133aa351
youssef el hadraoui <93286066+yel-hadr@users.noreply.github.com>
2023-01-31 12:47:17 +0100

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-hadr <yel-hadr@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/05 19:14:55 by yel-hadr          #+#    #+#             */
+/*   Updated: 2022/11/05 19:14:58 by yel-hadr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strdup(const char *s);
+void	*ft_calloc(size_t count, size_t size);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+char	*get_next_line(int fd);
+
+#endif
